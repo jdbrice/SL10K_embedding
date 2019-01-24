3c0cdded3c431dd41cfb249014650978fa4e7276
jdb12@rice.edu <jdb12@rice.edu>
2019-01-24 01:54:20 -0500

    Setting up scripts for vertex finding and another set for MC Vertex (normal embedding)

--- embed_ee.sh
+++ embed_ee.sh
@@ -1,6 +1,18 @@
-root4star -b <<EEE
+#!/bin/tcsh
+
+set nEvts=$1
+#set nEvts=5
+echo "Running on $nEvts Events"
+echo "#################### STARSIM #######################"
+starver SL16d
+root4star -b -q -l 'starsim.C('"$nEvts"')' >& starsim.log
+
+echo "#################### Embedding BFC Chain #######################"
+starver SL10k_embed
+echo $STAR
+root4star -b > bfcMixer.log <<EEE
   .L bfcMixer_Tpx.C
-  bfcMixer_Tpx(200,"/star/data03/pwg/jdb/upc/zerobias/st_zerobias_adc_11057034_raw_1590001.daq","st_zerobias_adc_11057034_raw_1590001.tags.root", 1, 5, -0.1, 0.1, -10, 10, 10, 8, 2, 0,
-               "P10ikAuAu200", "FlatPt", 1, "output.fzd");
-.q 
+  bfcMixer_Tpx($nEvts,"$SCRATCH/st_zerobias.daq","", 1, 5, -0.1, 0.1, -10, 10, 10, 8, 2, 0,
+               "P10ikAuAu200", "FlatPt", 1, "pythia6.starsim.fzd");
+  .q
 EEE
--- embed_ee_vtx.sh
+++ embed_ee_vtx.sh
@@ -10,14 +10,8 @@ root4star -b -q -l 'starsim.C('"$nEvts"')' >& starsim.log
 echo "#################### Embedding BFC Chain #######################"
 starver SL10k_embed
 echo $STAR
-#root4star -b > bfcVtx.log <<EEE
-#  .L bfcVtx.C
-#  bfcVtx( "$SCRATCH/st_zerobias.daq", $1 );
-#  .q 
-#EEE
-
 root4star -b > bfcMixer.log <<EEE
-  .L bfcMixer_Tpx.C
+  .L bfcVtx.C
   bfcMixer_Tpx($nEvts,"$SCRATCH/st_zerobias.daq","", 1, 5, -0.1, 0.1, -10, 10, 10, 8, 2, 0,
                "P10ikAuAu200", "FlatPt", 1, "pythia6.starsim.fzd");
   .q
